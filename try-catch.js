91039890d24ce1c95ba0a41e3116b2d2de8fe64a
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-23 19:22:48 +0600

    try catch error

---
+++ try-catch.js
@@ -0,0 +1,33 @@
+// try {
+
+//     console.log(666);
+// }
+// catch (error) {
+
+//     console.log(error)
+// }
+// finally {
+
+//     console.log('finally Done')
+// }
+
+
+
+try {
+
+    console.log('inside try');
+}
+catch (error) {
+    console.log('inside Catch');
+
+
+    console.log(error)
+}
+finally {
+
+    console.log('finally Done')
+}
+
+
+
+
