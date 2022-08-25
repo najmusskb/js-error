708ecbbed1671676f64cbc60fa41984b2c014629
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-25 20:41:18 +0600

    find

---
+++ find.js
@@ -0,0 +1,14 @@
+const array1 = [5, 12, 8, 130, 44];
+
+const found = array1.find(element => element % 5 === 0);
+const fiveAll = array1.filter(num => num % 5 === 0)
+
+console.log(found);
+console.log(fiveAll);
+
+
+
+
+
+
+
